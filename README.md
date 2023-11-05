9f29fc4999b975fc74850076f2637d36ddd00265
Aleksey Grishkin <kvgyzqk1996@yandex.ru>
2023-11-06 00:19:14 +0300

    Первая версия работы

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Проектная работа Mesto
 
+https://github.com/AlexGrishkin/mesto-project-ff.git
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,24 @@
 // @todo: Темплейт карточки
-
+const cardTemplate = document.querySelector("#card-template").content;
 // @todo: DOM узлы
-
+const container = document.querySelector(".places__list");
 // @todo: Функция создания карточки
-
+function addCard(dataCard, deleteFunction) {
+  const cardElement = cardTemplate
+    .querySelector(".places__item")
+    .cloneNode(true);
+  cardElement.querySelector(".card__image").src = dataCard.link;
+  cardElement.querySelector(".card__title").textContent = dataCard.name;
+  container.append(cardElement);
+  cardElement
+    .querySelector(".card__delete-button")
+    .addEventListener("click", deleteFunction);
+}
 // @todo: Функция удаления карточки
-
+const deleteCard = function (evt) {
+  evt.target.closest(".places__item").remove();
+};
 // @todo: Вывести карточки на страницу
+initialCards.forEach(function (element) {
+  addCard(element, deleteCard);
+});
